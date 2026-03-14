b8f852f92e5e41b275f4705ca9cf473b3675ea97
thmsm17 <thsme077@gmail.com>
2026-03-14 19:32:08 +0100

    Update artifacts/dakservice/src/pages/Home.tsx

--- artifacts/dakservice/src/pages/Home.tsx
+++ artifacts/dakservice/src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { Footer } from "@/components/layout/Footer";
 import { Hero } from "@/components/sections/Hero";
 import { Services } from "@/components/sections/Services";
 import { About } from "@/components/sections/About";
+import { Reviews } from "@/components/sections/Reviews";
 import { Contact } from "@/components/sections/Contact";
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
         <Hero />
         <Services />
         <About />
+        <Reviews />
         <Contact />
       </main>
       <Footer />
